21348701a0272539a8be0050c5467df533fd66be
Naima Gul <naimagul018@gmail.com>
2025-12-02 11:58:29 +0500

    Create HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Branch!");
+        System.out.println("Registration: FA22-BSE-018");
+    }
+}
